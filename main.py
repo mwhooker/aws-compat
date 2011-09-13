d9e3532754832b12bc8e99cd0f6f7f2d1f3f3d11
Matthew Hooker <matt@cloudscaling.com>
2011-09-12 20:43:11 -0400

    initial pass at validator.

---
+++ main.py
@@ -0,0 +1,70 @@
+from uuid import uuid4
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+AWS_KEY = ''
+AWS_SECRET = ''
+
+
+class TestS3Bucket(object):
+
+    def setUp(self):
+        self.conn = S3Connection(AWS_KEY, AWS_SECRET)
+        self.bucket_name = 'test_aws_conformance_' + uuid4().hex
+
+    def pre(self):
+        self.bucket = self.conn.create_bucket(self.bucket_name)
+
+    def pre_condition(self):
+
+        all_buckets = self.conn.get_all_buckets()
+        assert self.bucket_name in [bucket.name for bucket in all_buckets]
+
+    def post(self):
+        self.conn.delete_bucket(self.bucket_name)
+
+    def post_condition(self):
+
+        all_buckets = self.conn.get_all_buckets()
+        assert self.bucket_name not in [bucket.name for bucket in all_buckets]
+
+class TestS3Object(object):
+
+    def __init__(self, parent):
+        self.parent = parent
+
+    def setUp(self):
+        self.key = 'test_aws_conformance_' + uuid4().hex
+        self.value = uuid4().hex
+        self.k = Key(self.parent.bucket)
+        self.k.key = self.key
+
+    def pre(self):
+        self.k.set_contents_from_string(self.value)
+
+    def pre_condition(self):
+        assert self.k.get_contents_as_string() == self.value
+
+    def post(self):
+        self.k.delete()
+
+    def post_condition(self):
+        assert not self.k.get_contents_as_string()
+
+a = TestS3Bucket()
+b = TestS3Object(a)
+
+a.setUp()
+a.pre()
+a.pre_condition()
+
+
+b.setUp()
+b.pre()
+b.pre_condition()
+b.post()
+b.post_condition
+
+
+a.post()
+a.post_condition()
