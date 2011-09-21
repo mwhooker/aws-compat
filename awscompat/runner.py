fb65cb6a876e813edff1e0aaaeaaa892bb6c03dc
Matthew Hooker <matt@cloudscaling.com>
2011-09-21 18:17:15 -0400

    dead simple reporting.

--- awscompat/runner.py
+++ awscompat/runner.py
@@ -19,6 +19,8 @@ TODO:
 class Runner(object):
 
     def __init__(self, classes):
+        self.failures = []
+        self.errors = []
         self.branches = defaultdict(list)
 
         for klass in classes:
@@ -34,15 +36,47 @@ class Runner(object):
             obj.setUp()
             try:
                 obj.pre()
-                obj.pre_condition()
             except Exception, e:
-                print "pre exception: %s" % e
+                self.pre_error(obj, e)
+                continue
+
+            try:
+                obj.pre_condition()
+            except AssertionError, e:
+                self.pre_failure(obj, e)
                 continue
 
             self.run(klass, obj)
 
             try:
                 obj.post()
-                obj.post_condition()
             except Exception, e:
-                print "post exception: %s" % e
+                self.post_error(obj, e)
+                continue
+
+            try:
+                obj.post_condition()
+            except AssertionError, e:
+                self.pos_failure(obj, e)
+
+    def pre_failure(self, obj, e):
+        self.failures.append((obj, e, 'pre'))
+
+    def pre_error(self, obj, e):
+        self.errors.append((obj, e, 'pre'))
+
+    def post_failure(self, obj, e):
+        self.failures.append((obj, e, 'post'))
+
+    def post_error(self, obj, e):
+        self.errors.append((obj, e, 'post'))
+
+    def print_messages(self):
+        if len(self.errors):
+            print "errors: ", len(self.errors)
+
+        if len(self.failures):
+            print "failures: ", len(self.failures)
+
+        if not len(self.failures) and not len(self.errors):
+            print "OK"
