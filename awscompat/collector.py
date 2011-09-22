2fb5562e3b3f3ca97c9fddbd27027cce199543db
Matthew Hooker <matt@cloudscaling.com>
2011-09-22 04:28:54 -0400

    fix collector bug which caused no module to run in -m not given.

--- awscompat/collector.py
+++ awscompat/collector.py
@@ -5,12 +5,12 @@ import inspect
 import awscompat.tests.base as base
 
 
-def collect(dir, include_modules=[]):
+def collect(dir, include_modules=None):
     classes = set([])
 
     for path in glob.glob(os.path.join(dir, '*.py')):
         module_name = inspect.getmodulename(path)
-        if module_name not in include_modules:
+        if include_modules and module_name not in include_modules:
             continue
         module_name = 'awscompat.tests.' + module_name
         module = imp.load_source(module_name, path)
