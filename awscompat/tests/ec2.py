71483de15b0c3f1590c4aca4676216451e3b5c18
Matthew Hooker <matt@cloudscaling.com>
2011-09-21 23:25:45 -0400

    global connection object

--- awscompat/tests/ec2.py
+++ awscompat/tests/ec2.py
@@ -2,24 +2,28 @@ from boto.ec2.connection import EC2Connection
 
 from base import TestNode
 
+
+EC2_CONN = EC2Connection()
+
 class TestSecurityGroups(TestNode):
 
     def setUp(self):
-        self.conn = EC2Connection()
         self.group_name = self.make_key('group_name')
         self.group_desc = self.make_key('group_desc')
 
     def pre(self):
-        self.group = self.conn.create_security_group(
+        self.group = EC2_CONN.create_security_group(
             self.group_name,
             self.group_desc
         )
 
     def pre_condition(self):
-        assert repr(self.group) in [repr(g) for g in self.conn.get_all_security_groups()]
+        assert repr(self.group) in [repr(g) for g in
+                                    EC2_CONN.get_all_security_groups()]
 
     def post(self):
         self.group.delete()
 
     def post_condition(self):
-        assert repr(self.group) not in [repr(g) for g in self.conn.get_all_security_groups()]
+        assert repr(self.group) not in [repr(g) for g in
+                                        EC2_CONN.get_all_security_groups()]
