0c51729ecc5f9faca0d24cdba6c48e8fd349deca
Matthew Hooker <matt@cloudscaling.com>
2011-09-26 19:39:49 -0400

    config file cmdline opt.

--- run_tests.py
+++ run_tests.py
@@ -11,16 +11,24 @@ from awscompat import collector
 from awscompat import runner
 
 
-config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-
-with open(config_path) as f:
-    awscompat.config = json.load(f)
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', nargs='+', help='Only run these test modules.')
+parser.add_argument('-m', metavar='MODULE', nargs='+',
+                    help='Only run these test modules.')
+parser.add_argument('-c', default='config.json', metavar='CONFIG_FILE',
+                    help='Use alternate config file. Relative path. '
+                   'Defaults to config.json')
 args = parser.parse_args()
 
+config_path = os.path.join(os.path.dirname(__file__), args.c)
+try:
+    with open(config_path) as f:
+        awscompat.config = json.load(f)
+except IOError:
+    print "Please make sure a config file exists at %s" % config_path
+    sys.exit(-1)
+
 test_dir = os.path.join(os.path.dirname(__file__), 'awscompat', 'tests')
+
 
 classes = collector.collect(test_dir, include_modules=args.m)
 runner = runner.Runner(classes)
