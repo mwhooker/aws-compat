6d41f6756a90bd103fb60d59b49e7aa3c4305bca
Matthew Hooker <matt@cloudscaling.com>
2011-10-26 21:46:06 -0400

    some user friendlyness.

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -44,29 +44,16 @@ def update_env(env_mapping):
     base['ec2']['url'] = os.getenv('EC2_URL')
     base['s3']['url'] = os.getenv('S3_URL')
 
-novarc_keys = [
-    "EC2_ACCESS_KEY",
-    "EC2_SECRET_KEY",
-    "EC2_URL",
-    "S3_URL",
-    "EC2_USER_ID",
-    "EC2_PRIVATE_KEY",
-    "EC2_CERT",
-    "NOVA_CERT",
-    "EUCALYPTUS_CERT",
-    "NOVA_API_KEY",
-    "NOVA_USERNAME",
-    "NOVA_PROJECT_ID",
-    "NOVA_URL",
-    "NOVA_VERSION",
-]
+if not any([os.environ.has_key(key) for key in env_map['os'].values() + env_map['aws'].values()]):
+    sys.stderr.write("No access keys from the environment found. ")
+    sys.stderr.write("Try sourcing your credentials file.\n")
+    sys.exit(-1)
 
-
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser("Generate config file for awscompat. "
+                                 "By default, provide proper config for AWS.")
 parser.add_argument('provider', choices=['aws', 'os'],
                     help='generate config for amazon or openstack.')
 args = parser.parse_args()
-
 
 update_env(env_map[args.provider])
 
