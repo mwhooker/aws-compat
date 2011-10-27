6f2a6526e5bfaab614114d0025ecf77a8f2326cc
Matthew Hooker <matt@cloudscaling.com>
2011-10-27 19:05:28 -0400

    round one of connecting to openstack.

--- awscompat/connections.py
+++ awscompat/connections.py
@@ -1,6 +1,1 @@
-from boto.ec2.connection import EC2Connection
-from boto.s3.connection import S3Connection
-
-
-ec2_conn = EC2Connection()
-s3_conn = S3Connection()
+ec2_conn = s3_conn = None
--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python
 import argparse
+import boto
 import os.path
 import json
 import sys
+from urlparse import urlparse
+
+from boto.ec2.regioninfo import RegionInfo
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'third_party'))
 
 import awscompat
+import awscompat.connections
 from awscompat import collector
 from awscompat import runner
+
+def split_clc_url(clc_url):
+    """
+    Splits a cloud controller endpoint url.
+    """
+    parts = urlparse(clc_url)
+    is_secure = parts.scheme == 'https'
+    ip, port = parts.netloc.split(':')
+    return {
+        'ip': ip,
+        'port': int(port),
+        'is_secure': is_secure,
+        'path': parts.path
+    }
 
 
 parser = argparse.ArgumentParser()
@@ -19,6 +38,7 @@ parser.add_argument('-c', default='config.json', metavar='CONFIG_FILE',
                    'Defaults to config.json')
 args = parser.parse_args()
 
+
 config_path = os.path.join(os.path.dirname(__file__), args.c)
 try:
     with open(config_path) as f:
@@ -27,8 +47,32 @@ except IOError:
     print "Please make sure a config file exists at %s" % config_path
     sys.exit(-1)
 
-test_dir = os.path.join(os.path.dirname(__file__), 'awscompat', 'tests')
 
+parts = split_clc_url(awscompat.config['ec2']['url'])
+
+awscompat.connections.ec2_conn = boto.connect_ec2(
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    is_secure=parts['is_secure'],
+    region=RegionInfo(None,
+                      'nova',
+                      parts['ip']),
+    port=parts['port'],
+    path=parts['path']
+)
+
+awscompat.connections.s3_conn = boto.connect_s3(
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    is_secure=parts['is_secure'],
+    region=RegionInfo(None,
+                      'nova',
+                      parts['ip']),
+    port=parts['port'],
+    path=parts['path']
+)
+
+test_dir = os.path.join(os.path.dirname(__file__), 'awscompat', 'tests')
 
 classes = collector.collect(test_dir, include_modules=args.m)
 runner = runner.Runner(classes)
