c3a45be233e7e6abf8ec28d13127916741b7a3c6
Matthew Hooker <matt@cloudscaling.com>
2011-10-26 21:41:13 -0400

    better config generator.

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -9,6 +9,8 @@ except ImportError:
 from itertools import ifilter
 
 base = {
+    'access_key': None,
+    'secret': None,
     'ec2': {
         'url': None,
         "test_image_id": "ami-8e1fece7",
@@ -23,6 +25,24 @@ base = {
         'url': None
     }
 }
+
+env_map = {
+    'os': {
+        'access_key': 'EC2_ACCESS_KEY',
+        'secret': 'EC2_SECRET_KEY'
+    },
+    'aws': {
+        'access_key': 'AWS_ACCESS_KEY_ID',
+        'secret': 'AWS_SECRET_ACCESS_KEY'
+    }
+}
+
+def update_env(env_mapping):
+    for key in env_mapping:
+        base[key] = os.getenv(env_mapping[key])
+
+    base['ec2']['url'] = os.getenv('EC2_URL')
+    base['s3']['url'] = os.getenv('S3_URL')
 
 novarc_keys = [
     "EC2_ACCESS_KEY",
@@ -43,12 +63,11 @@ novarc_keys = [
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--env', action='store_true',
-                    help='Generate config from environment.')
+parser.add_argument('provider', choices=['aws', 'os'],
+                    help='generate config for amazon or openstack.')
 args = parser.parse_args()
 
 
-if args.env:
-    base.update(load_env())
+update_env(env_map[args.provider])
 
 print json.dumps(base)
